77b0d84198480c9270eed67b449a106ed825688d
jiyounglee-0523 <jiyounglee0523@gmail.com>
2025-06-13 16:30:24 +0900

    remove safety blocks

--- src/run/benchmark_eval.py
+++ src/run/benchmark_eval.py
@@ -172,11 +172,41 @@ def gemini_call(
         max_tokens=2048,
         max_retries=5,
     ):
+
+    safety_settings=[
+    {
+        "category": "HARM_CATEGORY_DANGEROUS",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "OTHER",
+        "threshold": "BLOCK_NONE",
+    }
+    ]
     
     model = genai.GenerativeModel(
             args.model,
             system_instruction="Do not reason for too long. If the question is a multiple choice question, answer with the option letter. If none of the given options match, you may guess or say 'none of the above.' Start your final sentence with 'The answer is '.",
+            safety_settings=safety_settings,
     )
+
+
     
     noanswer_indices = []
 
