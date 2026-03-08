17e9a273b747006126b17c95acf92c0da8452177
l-serena <serenaliu@college.harvard.edu>
2026-03-07 19:33:18 -0500

    Update common.py

--- src/utils/common.py
+++ src/utils/common.py
@@ -5,40 +5,42 @@ from framework.data_return import *
 from registry.dataset_map import LOAD_TEST_DATASET
 
 
-
-def exponential_backoff(retry_count:int,
-                        base_delay:int=5,
-                        max_delay:int=65,
-                        jitter:bool=True) -> float:
+def exponential_backoff(
+    retry_count: int, base_delay: int = 5, max_delay: int = 65, jitter: bool = True
+) -> float:
     """
     Exponential backoff function for API calling.
 
     Args:
         retry_count (int): Retry count.
         base_delay (int, optional): Base delay seconds. Defaults to 5.
-        max_delay (int, optional): Maximum delay seconds. Defaults to 165.
+        max_delay (int, optional): Maximum delay seconds. Defaults to 65.
         jitter (bool, optional): Whether apply randomness. Defaults to True.
 
     Returns:
         float: Final delay time.
     """
-    delay = min(base_delay * (2 ** retry_count), max_delay)
+    delay = min(base_delay * (2**retry_count), max_delay)
     if jitter:
         delay = random.uniform(delay * 0.8, delay * 1.2)
     return delay
 
 
-
 def save_func(to_save, save_config, dataset_config, generation_config, task_config):
     save_mapping = {
-        'mmlu': return_mmlu,
-        'gsm8k': return_gsm8k,
-        'arc': return_arc,
-        'hellaswag': return_hellaswag,
-        'truthfulqa': return_truthfulqa,
-        'winogrande': return_winogrande,
+        "mmlu": return_mmlu,
+        "gsm8k": return_gsm8k,
+        "arc": return_arc,
+        "hellaswag": return_hellaswag,
+        "truthfulqa": return_truthfulqa,
+        "winogrande": return_winogrande,
+
+        # Open-ended datasets
+        "ifeval": return_openended,
+        "alpacafarm": return_openended,
+        "mt-bench": return_openended,
     }
-    
+
     rerun_index = None
     cefr_index = None
 
@@ -46,8 +48,15 @@ def save_func(to_save, save_config, dataset_config, generation_config, task_conf
 
     if generation_config.rerun is not None:
         rerun_index = list(np.load(generation_config.rerun))
-    if task_config.task_name in ['cefr', 'L1']:
-        cefr_index = list(np.load(f'assets/vocab_processed/{dataset_config.dataset_name}_{(task_config.cefr_level).lower()}.npy'))
+
+    if task_config.task_name in ["cefr", "L1"]:
+        cefr_index = list(
+            np.load(
+                f"assets/vocab_processed/{dataset_config.dataset_name}_{(task_config.cefr_level).lower()}.npy"
+            )
+        )
         cefr_index = [int(index) for index in cefr_index]
 
-    return save_mapping[dataset_config.dataset_name](test_dataset, to_save, save_config, rerun_index, cefr_index)
+    return save_mapping[dataset_config.dataset_name](
+        test_dataset, to_save, save_config, rerun_index, cefr_index
+    )
